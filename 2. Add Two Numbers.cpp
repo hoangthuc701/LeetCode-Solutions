7f2c8098f2b3bc781dc0c756489f7b4c6e980ec1
Hoang Thuc <hoangthuc701@gmail.com>
2021-07-24 10:32:56 +0700

    2. Add Two Numbers

---
+++ 2. Add Two Numbers.cpp
@@ -0,0 +1,26 @@
+// Author: Nguyen Hoang Thuc
+// Link: https://leetcode.com/problems/add-two-numbers/
+
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* p= l1, *q=l2, *curr = dummyHead; 
+        int carry =0;
+        while (p != NULL || q!=NULL){
+            int x= (p!=NULL )? p->val :0;
+            int y = (q!=NULL)? q->val:0;
+            int sum = x+y+carry;
+            carry = sum/10;
+            curr-> next = new ListNode(sum%10);
+            curr= curr->next;
+            if (p!=NULL) p = p->next;
+            if (q!=NULL) q= q->next;
+        }
+        if (carry >0){
+            curr->next = new ListNode(carry);
+        }
+        return dummyHead->next;
+    }
+};
